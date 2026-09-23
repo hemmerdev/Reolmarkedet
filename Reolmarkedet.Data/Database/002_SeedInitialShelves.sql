045ab4034a798047e4618870a3c182c4ba5e1f7b
Andreas Hemmer <andreas.hemmer@hotmail.com>
2026-09-23 16:10:41 +0200

    Add SQL script to seed initial shelf data
    Added a script to seed up to 80 shelves in ReolmarkedetDB. The script checks for the existence of two shelf types, retrieves their IDs, and inserts shelves accordingly: 60 with "6 hylder" and 20 with "3 hylder og bøjlestang". Includes validation to ensure required shelf types exist before inserting data.

---
+++ Reolmarkedet.Data/Database/002_SeedInitialShelves.sql
@@ -0,0 +1,43 @@
+﻿USE ReolmarkedetDB
+GO
+
+DECLARE @SixShelvesTypeID INT;
+DECLARE @ClothesRailTypeID INT;
+
+SELECT @SixShelvesTypeID = ShelfTypeID 
+FROM dbo.SHELFTYPE 
+WHERE ShelfTypeName = N'6 hylder';
+
+SELECT @ClothesRailTypeID = ShelfTypeID
+FROM dbo.SHELFTYPE 
+WHERE ShelfTypeName = N'3 hylder og bøjlestang';
+
+IF @SixShelvesTypeID IS NULL OR @ClothesRailTypeID IS NULL
+BEGIN
+	PRINT N'De nødvendige hyldetyper findes ikke.';
+	RETURN;
+END;
+
+DECLARE @ShelfNumber INT = 1;
+DECLARE @ShelfTypeID INT;
+
+WHILE @ShelfNumber <= 80
+BEGIN
+	IF @ShelfNumber <= 60
+		SET @ShelfTypeID = @SixShelvesTypeID;
+	ELSE
+		SET @ShelfTypeID = @ClothesRailTypeID;
+
+
+	IF NOT EXISTS (
+		SELECT 1 
+		FROM dbo.SHELF 
+		WHERE ShelfNumber = @ShelfNumber)
+	BEGIN
+		INSERT INTO dbo.SHELF (ShelfNumber, IsActive, ShelfTypeID)
+		VALUES (@ShelfNumber, 1, @ShelfTypeID);
+	END;
+
+	SET @ShelfNumber = @ShelfNumber + 1;
+END;
+
